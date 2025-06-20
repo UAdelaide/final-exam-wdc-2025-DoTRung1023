4350a5aa2a6898fd1e82913a0812f302ba5258a4
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 10:39:02 +0000

    [automatic commit] 2025-06-20 10:39:02 UTC 129.127.202.127

--- part2/routes/dogsRoutes.js
+++ part2/routes/dogsRoutes.js
@@ -5,7 +5,7 @@ const db = require('../models/db');
 router.get('/dogs', async function(req, res, next) {
   try {
     const [rows] = await db.query(`
-      select * from Dogs
+      select * from Users
     `);
     res.json(rows);
   } catch (error) {
