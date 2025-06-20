0cfdc3944456ef5f4abd1568074f6c6a631ff9f3
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 02:06:03 +0000

    [automatic commit] 2025-06-20 02:06:03 UTC 129.127.202.127

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var 
+var dogsRouter = require('./routes/dogs');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dogs', dogsRouter);
 
 module.exports = app;
--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,24 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/dogs', async function(req, res, next) {
-  try {
-    const [rows] = await db.query(`
-      SELECT d.name AS dog_name, d.size, u.username AS owner_name
-      FROM Dogs d
-      JOIN Users u ON d.owner_id = u.user_id
-    `);
-    res.json(rows);
-  } catch (error) {
-    console.error('Database error:', error);
-    res.status(500).json({ error: 'Failed to fetch dogs' });
-  }
 });
 
 module.exports = router;
