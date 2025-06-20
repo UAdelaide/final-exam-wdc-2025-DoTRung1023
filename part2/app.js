8971efd1e8e50093fef89fda452bd52d0fb863c5
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 09:43:18 +0000

    [automatic commit] 2025-06-20 09:43:18 UTC 129.127.202.127

--- part2/app.js
+++ part2/app.js
@@ -24,7 +24,7 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-
+cons
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
