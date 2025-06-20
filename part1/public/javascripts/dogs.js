2b246c001cd0058912edfde28839b9b49d3223a4
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 04:44:22 +0000

    [automatic commit] 2025-06-20 04:44:21 UTC 129.127.202.127

--- part1/public/javascripts/dogs.js
+++ part1/public/javascripts/dogs.js
@@ -11,7 +11,13 @@ createApp({
     },
     methods: {
         async fetchNewDog(){
-
+            try {
+                const response = await fetch('https://dog.ceo/api/breeds/image/random');
+                const data = await response.json();
+                this.dogImage = data.message;
+            } catch (error) {
+                this.dogImage = 'https://via.placeholder.com/400x300?text=Dog+Photo';
+            }
         }
     }
 }).mount('#app');
