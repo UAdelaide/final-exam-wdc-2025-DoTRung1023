f8ad914ef377b2ff622c2a72bc2fa901dff5b2c7
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 02:07:06 +0000

    [automatic commit] 2025-06-20 02:07:06 UTC 129.127.202.127

--- part1/db.js
+++ part1/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'DogWalkService',
+  database: 'DogWalkService'
 });
 
 module.exports = db;
