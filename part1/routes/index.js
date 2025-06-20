db81fc6c656a6110cda958d3cedb680abcdf2bef
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 02:02:35 +0000

    [automatic commit] 2025-06-20 02:02:35 UTC 129.127.202.127

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -7,7 +7,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/dogs', function(req, res, next)) {
-}
+router.get('/dogs', async function(req, res, next) {
+  try {
+    const [rows] = await db.execute(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_name
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+    res.json(rows);
+  } catch (error) {
+    console.error('Database error:', error);
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
 
 module.exports = router;
