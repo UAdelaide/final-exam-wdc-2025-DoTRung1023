bdfdbae2fac265bfacd2738d413bcba734d1171e
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 02:07:52 +0000

    [automatic commit] 2025-06-20 02:07:52 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.127

--- part1/app.js
+++ part1/app.js
@@ -17,6 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/dogs', dogsRouter);
+app.use('/api', dogsRouter);
 
 module.exports = app;
