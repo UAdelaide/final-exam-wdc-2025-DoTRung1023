1c84ca0e95bc7f4eb42a906b8dd6e84dbf2e4714
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 04:45:25 +0000

    [automatic commit] 2025-06-20 04:45:25 UTC 129.127.202.127

--- part1/public/javascripts/dogs.js
+++ part1/public/javascripts/dogs.js
@@ -6,6 +6,8 @@ createApp({
             dogImage: '',
             dogName: '',
             dogBreed: '',
+            dogAge: '',
+            dogDescription: ''
         }
     },
     mounted(){
