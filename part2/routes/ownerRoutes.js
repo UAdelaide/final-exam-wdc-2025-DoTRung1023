6589e390d981717332d040d49b3b217612f01581
DoTRung1023 <a1899443@adelaide.edu.au>
2025-06-20 09:44:00 +0000

    [automatic commit] 2025-06-20 09:44:00 UTC 129.127.202.127

--- part2/routes/ownerRoutes.js
+++ part2/routes/ownerRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const db = require('../models/db');
 
 // GET all users (for admin/testing)
-router.get('/', async (req, res) => {
+router.get('/dogs', async (req, res) => {
   try {
-    const [rows] = await db.query('SELECT user_id, username, email, role FROM Users');
+    const [rows] = await db.query('SELECT dog_id, name, size FROM Dogs WHERE owner_id = ?', [req.session.user.user_id]);
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch users' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 
